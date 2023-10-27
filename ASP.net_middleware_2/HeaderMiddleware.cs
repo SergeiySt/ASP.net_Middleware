ca5272503bedacfff5df199518738245b3abdc24
SergeiySt <ser.st.1989@gmail.com>
2023-10-27 21:04:00 +0300

    add_77

---
+++ ASP.net_middleware_2/HeaderMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace ASP.net_middleware_2
+{
+    public class HeaderMiddleware
+    {
+        public async Task Invoke(HttpContext context, Func<Task> next)
+        {
+            foreach (var header in context.Request.Headers)
+            {
+                Debug.WriteLine($"Header: {header.Key}: {header.Value}");
+            }
+
+            await next();
+        }
+    }
+}
---
+++ ASP.net_middleware_3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.net_middleware_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
